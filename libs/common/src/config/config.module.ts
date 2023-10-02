6e01315e187ca997e38c0b642360a87015bb2a02
AliSootoodeh <alisotudehdev@gmail.com>
2023-10-02 12:55:09 +0330

    adding Joi and implementing Config Service

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule,
+} from '@nestjs/config';
+import * as Joi from 'joi';
 
-@Module({})
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({ MONGODB_URI: Joi.string() }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
 export class ConfigModule {}
